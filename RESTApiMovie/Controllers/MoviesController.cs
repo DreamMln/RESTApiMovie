07020e6af378f63ec5b850b39bdf95d985266bc7
DreamMln <maix2374@edu.zealand.dk>
2022-06-05 13:28:10 +0200

    Add project files.

---
+++ RESTApiMovie/Controllers/MoviesController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using MovieDLL;
+using RESTApiMovie.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RESTApiMovie.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        //ref til manager
+        private MoviesManager _manager = new MoviesManager();
+        // GET: api/<MoviesController>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet]
+        public ActionResult<IEnumerable<Movie>> Get()
+        {
+            IEnumerable<Movie> result = _manager.GetAll();
+            if (result.Count() == 0)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+
+        // GET api/<MoviesController>/5
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet("{id}")]
+        public ActionResult<Movie> Get(int id)
+        {
+            Movie result = _manager.GetById(id);
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+
+        }
+
+        // POST api/<ValuesController>
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [HttpPost]
+        public ActionResult<Movie> Post([FromBody] Movie addMovie)
+        {
+            Movie result = _manager.AddMovie(addMovie);
+            if (result == null)
+            {
+                return null;
+            }
+            return Created($"/api/movies/{result.Id}", result);
+        }
+
+        //// PUT api/<ValuesController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<ValuesController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ RESTApiMovie/Managers/MoviesManager.cs
@@ -0,0 +1,47 @@
+﻿using MovieDLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTApiMovie.Managers
+{
+    public class MoviesManager
+    {
+        private static int _nextID = 1;
+
+        private static List<Movie> _movies = new List<Movie>()
+        {
+            new Movie() { Id = _nextID++, Name = "Harry Potter", Country = "America", LengthInMinutes = 152 },
+            new Movie() { Id = _nextID++, Name = "Lord og the Rings", Country = "New Zealand" , LengthInMinutes = 178 },
+            new Movie() { Id = _nextID++, Name = "Hobitten", Country = "America", LengthInMinutes = 169 }
+        };
+
+        public IEnumerable<Movie> GetAll()
+        {
+            List<Movie> result = new List<Movie>(_movies);
+            return result;
+        }
+
+        public Movie GetById(int id)
+        {
+            if (id == null)
+            {
+                return null;
+            }
+            Movie movie = _movies.Find(m => m.Id == id);
+            return movie;
+        }
+
+        public Movie AddMovie(Movie newMovie)
+        {
+            if (newMovie == null)
+            {
+                return null;
+            }
+            newMovie.Id = _nextID++;
+            _movies.Add(newMovie);
+            return newMovie;
+        }
+    }
+}
